294b25ae5a0f36d983cbc52d8e0044591075fb50
Irirwanirira <irijoseph41@gmail.com>
2022-12-13 19:53:54 +0200

    Update table

--- vet_clinic.sql
+++ vet_clinic.sql
@@ -36,3 +36,44 @@ SELECT names,escape_attempts from animals where weight_kg > 10.5;
 SELECT * from animals where neutered =true;
 SELECT * from animals where names!='Gabumon.' ;
 SELECT * from animals where weight_kg>=10.4 AND weight_kg<=17.3;
+
+
+-- Updates
+
+UPDATE animals SET escape_attempts =2 WHERE id = 2;
+UPDATE animals SET names = 'Gabumon' WHERE weight_kg = 8;
+
+
+-- Querries_updates
+
+ALTER TABLE animals ADD species TEXT;
+
+-- add DATA
+
+INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (5,' Charmander', '2020-02-08', '0' , FALSE, '-11');
+
+INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (6,' Plantom', '2021-11-15', '2' , TRUE, '-5.7');
+
+INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (7,' Squirtle', '1993-04-02', '3' , FALSE, '-12.13');
+
+INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (8,' Angemon', '2005-06-12', '1' , TRUE, '-45');
+
+INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (9,' Boarmon', '2005-06-07', '7' , TRUE, '20.4');
+
+INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (10,' Blossom', '1998-10-13', '3' , TRUE, '17');
+
+INSERT INTO animals (id, names, date_of_birth, escape_attempts, neutered, weight_kg)
+VALUES (11,' Ditto', '2022-04-014', '4' , TRUE, '22');
+
+
+
+
+
+
+
