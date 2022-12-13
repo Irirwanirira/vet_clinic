d05cc19306f6daf223381443906e89d80f552756
Irirwanirira <irijoseph41@gmail.com>
2022-12-13 12:13:39 +0200

    Add data in the table

--- vet_clinic.sql
+++ vet_clinic.sql
@@ -1,12 +1,16 @@
--- vet database with a table of animal and id as a unic value
+-- vet database with a table of animal and id as a PRIMARY KEY
 
 DROP TABLE IF EXISTS vet_clinic;
 
 CREATE TABLE animals (
-    id INT,
+    id INT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
     weight_kg DECIMAL
 );
+
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES ('Agumon', 'Feb 3rd, 2020', '0' , TRUE, '10.23' );
+
+
